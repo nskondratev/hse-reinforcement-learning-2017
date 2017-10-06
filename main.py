bc4a94385508a027779bc73396207e12002a0c93
Nikita Kondratev <highflyer16@yandex.ru>
2017-10-06 23:52:18 +0300

    Feature. Set learning rate to 0.01.

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., valu
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
                train_interval=4, delta_clip=1.)
-dqn.compile(Adam(lr=.00025), metrics=['mae'])
+dqn.compile(Adam(lr=.01), metrics=['mae'])
 
 if args.mode == 'train':
     weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
