c051cf69ca83271bcd3d09fce39724b3344d92da
Nikita Kondratev <highflyer16@yandex.ru>
2017-10-23 19:03:51 +0300

    Refactor. Get environment from args.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from keras.optimizers import Adam
 import keras.backend as K
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
@@ -18,16 +18,15 @@ from atari_processor import AtariProcessor
 INPUT_SHAPE = (84, 84)
 WINDOW_LENGTH = 4
 
-ENV_NAME = 'Tennis-v0'
-
 # Parse command line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument('--env', choices=['Robotank-v0', 'Boxing-v0', 'Tennis-v0', 'Pong-v0'], default='Boxing-v0')
 parser.add_argument('--mode', choices=['train', 'test'], default='train')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
 # Get the environment and extract the number of actions.
-env = gym.make(ENV_NAME)
+env = gym.make(args.env)
 np.random.seed(123)
 env.seed(123)
 nb_actions = env.action_space.n
@@ -63,21 +62,18 @@ processor = AtariProcessor()
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
                               nb_steps=1000000)
 
-# Alternative policy
-# policy = BoltzmannQPolicy(tau=1.)
-
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
                train_interval=4, delta_clip=1.)
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
 if args.mode == 'train':
-    weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
-    checkpoint_weights_filename = 'dqn_' + ENV_NAME + '_weights_{step}.h5f'
-    log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
+    weights_filename = 'dqn_{}_weights.h5f'.format(args.env)
+    checkpoint_weights_filename = 'dqn_' + args.env + '_weights_{step}.h5f'
+    log_filename = 'dqn_{}_log.json'.format(args.env)
     callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
     callbacks += [FileLogger(log_filename, interval=100)]
-    dqn.fit(env, callbacks=callbacks, nb_steps=10000000, log_interval=10000)
+    dqn.fit(env, callbacks=callbacks, nb_steps=3000000, log_interval=10000)
 
     # Save final weights
     dqn.save_weights(weights_filename, overwrite=True)
@@ -85,7 +81,7 @@ if args.mode == 'train':
     # Evaluate our algorithm for 10 episodes.
     dqn.test(env, nb_episodes=10, visualize=False)
 elif args.mode == 'test':
-    weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
+    weights_filename = 'dqn_{}_weights.h5f'.format(args.env)
     if args.weights:
         weights_filename = args.weights
     dqn.load_weights(weights_filename)
